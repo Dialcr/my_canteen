b07b3d0ea6d357cba7fec0557abb1ead5eb85989
Diego Alfonso Criado <dialcr45@gmail.com>
2024-02-29 10:34:38 -0500

    add migrtation to drop column price from Product table

--- Canteen/src/Canteen.DataAccess/Migrations/20240229153355_drop_price_colum_from_product_Table.Designer.cs
+++ Canteen/src/Canteen.DataAccess/Migrations/20240229153355_drop_price_colum_from_product_Table.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Canteen.DataAccess;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Canteen.DataAccess.Migrations
 {
     [DbContext(typeof(EntitiesContext))]
-    partial class EntitiesContextModelSnapshot : ModelSnapshot
+    [Migration("20240229153355_drop_price_colum_from_product_Table")]
+    partial class drop_price_colum_from_product_Table
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
