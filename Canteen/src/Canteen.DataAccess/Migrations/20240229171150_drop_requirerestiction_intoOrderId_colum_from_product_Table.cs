e35fc37764b9a5af057f2e7c6a1b7772e7c4eefb
Diego Alfonso Criado <dialcr45@gmail.com>
2024-02-29 12:12:27 -0500

    add migratino to drop required restriction into orderid column from request table

---
+++ Canteen/src/Canteen.DataAccess/Migrations/20240229171150_drop_requirerestiction_intoOrderId_colum_from_product_Table.cs
@@ -0,0 +1,100 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Canteen.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class drop_requirerestiction_intoOrderId_colum_from_product_Table : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Requests_Carts_CartId",
+                table: "Requests");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Requests_Orders_OrderId",
+                table: "Requests");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "OrderId",
+                table: "Requests",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "CartId",
+                table: "Requests",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Requests_Carts_CartId",
+                table: "Requests",
+                column: "CartId",
+                principalTable: "Carts",
+                principalColumn: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Requests_Orders_OrderId",
+                table: "Requests",
+                column: "OrderId",
+                principalTable: "Orders",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Requests_Carts_CartId",
+                table: "Requests");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Requests_Orders_OrderId",
+                table: "Requests");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "OrderId",
+                table: "Requests",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "CartId",
+                table: "Requests",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Requests_Carts_CartId",
+                table: "Requests",
+                column: "CartId",
+                principalTable: "Carts",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Requests_Orders_OrderId",
+                table: "Requests",
+                column: "OrderId",
+                principalTable: "Orders",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
