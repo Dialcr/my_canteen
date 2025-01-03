16fab97c157ea6bf26b084d87d3da83ba95a7408
Diego Alfonso Criado <diego.ac@factory.generalsoftwareinc.com>
2025-01-03 14:08:29 -0500

    MIGRATION | add name into app user model

---
+++ Canteen/src/Canteen.DataAccess/Migrations/20250103183902_add-name-appUser.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Canteen.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class addnameappUser : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Name",
+                table: "AspNetUsers",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+            {
+                migrationBuilder.Sql(@"
+        UPDATE ""AspNetUsers"" 
+        SET ""Name"" = ""UserName"" 
+        WHERE ""Name"" = ''");
+            }
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Name",
+                table: "AspNetUsers");
+            migrationBuilder.Sql(@"
+        UPDATE ""AspNetUsers"" 
+        SET ""Name"" = '' ");
+
+        }
+    }
+}
