3f495bf7de4ad4cbe5a279721b805108823fea82
Diego Alfonso Criado <dialcr45@gmail.com>
2025-01-21 20:51:31 -0500

    fix(be): add establishment category images into output dto

--- Canteen/src/Canteen.Services/Dto/EstablishmentCategory/EstablishmentCategoryOutputDto.cs
+++ Canteen/src/Canteen.Services/Dto/EstablishmentCategory/EstablishmentCategoryOutputDto.cs
@@ -9,6 +9,8 @@ public class EstablishmentCategoryOutputDto
     public required string Name { get; set; }
     public string Description { get; set; } = string.Empty;
     public string StatusBase { get; set; } = DataAccess.Enums.StatusBase.Active.ToString();
+    public string? Image { get; set; }
+
 
 
 }
@@ -22,7 +24,8 @@ public static class EstablishmentCategoryExtention
             Description = establishmentCategory.Description,
             Id = establishmentCategory.Id,
             Name = establishmentCategory.Name,
-            StatusBase = establishmentCategory.StatusBase.ToString()
+            StatusBase = establishmentCategory.StatusBase.ToString(),
+            Image = establishmentCategory.Image
         };
 
     }
