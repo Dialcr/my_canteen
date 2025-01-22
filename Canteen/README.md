97940ffaa7b8d332ede3eafee742954340960ecb
Diego Alfonso Criado <dialcr45@gmail.com>
2025-01-21 19:22:10 -0500

    MIGRATION | fix dietary restrictions

--- Canteen/README.md
+++ Canteen/README.md
@@ -10,12 +10,12 @@
 
 ---
 
-## dotnet ef migrations add <Migration-name> --project Canteen.DataAccess\ Canteen.DataAccess.csproj --startup-project Canteen.Api\ Canteen.Api.csproj
+## dotnet ef migrations add <Migration-name> --project Canteen.DataAccess\Canteen.DataAccess.csproj --startup-project Canteen.Api\Canteen.Api.csproj
 
 # Canteen database update command
 
 ---
 
-# dotnet ef database update --project Canteen.DataAccess\ Canteen.DataAccess.csproj --startup-project Canteen.Api\ Canteen.Api.csproj
+# dotnet ef database update --project Canteen.DataAccess\Canteen.DataAccess.csproj --startup-project Canteen.Api\Canteen.Api.csproj
 
 ---
---
+++ Canteen/src/Canteen.DataAccess/Migrations/20250121235120_change-model.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Canteen.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class changemodel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_DietaryRestrictionProduct_DietaryRestriction_DietaryRestric~",
+                table: "DietaryRestrictionProduct");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_DietaryRestriction",
+                table: "DietaryRestriction");
+
+            migrationBuilder.RenameTable(
+                name: "DietaryRestriction",
+                newName: "DietaryRestrictions");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_DietaryRestrictions",
+                table: "DietaryRestrictions",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_DietaryRestrictionProduct_DietaryRestrictions_DietaryRestri~",
+                table: "DietaryRestrictionProduct",
+                column: "DietaryRestrictionsId",
+                principalTable: "DietaryRestrictions",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_DietaryRestrictionProduct_DietaryRestrictions_DietaryRestri~",
+                table: "DietaryRestrictionProduct");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_DietaryRestrictions",
+                table: "DietaryRestrictions");
+
+            migrationBuilder.RenameTable(
+                name: "DietaryRestrictions",
+                newName: "DietaryRestriction");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_DietaryRestriction",
+                table: "DietaryRestriction",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_DietaryRestrictionProduct_DietaryRestriction_DietaryRestric~",
+                table: "DietaryRestrictionProduct",
+                column: "DietaryRestrictionsId",
+                principalTable: "DietaryRestriction",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
