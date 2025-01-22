7cf5b756ebc09f16a1da47bb57a0809de96e497b
Diego Alfonso Criado <dialcr45@gmail.com>
2025-01-21 19:23:01 -0500

    MIGRATION | fix product image model

---
+++ Canteen/src/Canteen.DataAccess/Migrations/20250122002231_fix-product-image.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Canteen.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class fixproductimage : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_ProductImageUrls_Products_ProductId",
+                table: "ProductImageUrls");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "ProductId",
+                table: "ProductImageUrls",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "integer",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ProductImageUrls_Products_ProductId",
+                table: "ProductImageUrls",
+                column: "ProductId",
+                principalTable: "Products",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_ProductImageUrls_Products_ProductId",
+                table: "ProductImageUrls");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "ProductId",
+                table: "ProductImageUrls",
+                type: "integer",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "integer");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ProductImageUrls_Products_ProductId",
+                table: "ProductImageUrls",
+                column: "ProductId",
+                principalTable: "Products",
+                principalColumn: "Id");
+        }
+    }
+}
