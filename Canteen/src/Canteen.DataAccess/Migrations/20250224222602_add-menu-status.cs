4c4f8c987fe74e08353b8a7fbf2ecc1275c865a5
Diego Alfonso Criado <dialcr45@gmail.com>
2025-02-24 17:27:47 -0500

    MIGRATION | add menu status into model

---
+++ Canteen/src/Canteen.DataAccess/Migrations/20250224222602_add-menu-status.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Canteen.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class addmenustatus : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "StatusBase",
+                table: "Menus",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "StatusBase",
+                table: "Menus");
+        }
+    }
+}
