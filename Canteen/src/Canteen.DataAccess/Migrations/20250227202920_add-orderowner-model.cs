134b64d5a31ae84f03dd9720ebd03baa6d7ed3d8
Diego Alfonso Criado <dialcr45@gmail.com>
2025-02-27 15:31:07 -0500

    MIGRATION | add order owner model

---
+++ Canteen/src/Canteen.DataAccess/Migrations/20250227202920_add-orderowner-model.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace Canteen.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class addorderownermodel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Identifier",
+                table: "Orders",
+                type: "character varying(100)",
+                maxLength: 100,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<int>(
+                name: "OwnerId",
+                table: "Orders",
+                type: "integer",
+                nullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "OrderOwners",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Identifier = table.Column<string>(type: "text", nullable: false),
+                    Name = table.Column<string>(type: "text", nullable: false),
+                    Email = table.Column<string>(type: "text", nullable: false),
+                    PhoneNumber = table.Column<string>(type: "text", nullable: false),
+                    Address = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_OrderOwners", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Orders_OwnerId",
+                table: "Orders",
+                column: "OwnerId",
+                unique: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Orders_OrderOwners_OwnerId",
+                table: "Orders",
+                column: "OwnerId",
+                principalTable: "OrderOwners",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Orders_OrderOwners_OwnerId",
+                table: "Orders");
+
+            migrationBuilder.DropTable(
+                name: "OrderOwners");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Orders_OwnerId",
+                table: "Orders");
+
+            migrationBuilder.DropColumn(
+                name: "Identifier",
+                table: "Orders");
+
+            migrationBuilder.DropColumn(
+                name: "OwnerId",
+                table: "Orders");
+        }
+    }
+}
